ba7e2fe4eeae8edc3925ae93373650413f11baa2
Bombazook <bombazook@gmail.com>
2014-11-14 08:23:11 +0300

    added images to precompile
    fastfix

--- lib/imgareaselect/rails/engine.rb
+++ lib/imgareaselect/rails/engine.rb
@@ -1,6 +1,9 @@
 module Imgareaselect
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'imageareaselect-rails.assets.precompile' do |app|
+        app.config.assets.precompile += ["border-anim-h.gif", "border-anim-v.gif", "border-h.gif", "border-v.gif"]
+      end
     end
   end
 end
